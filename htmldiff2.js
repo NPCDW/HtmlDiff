3b1163b42b0c0950996e0b4414b50d35f884ba0c
cyl <cyl14082@cnki.net>
2021-08-28 14:41:22 +0800

    html对比支持生成一个或还原成两个html，并添加标记，支持添加到忽略列表的标签不进行对比

--- htmldiff2.js
+++ htmldiff2.js
@@ -1,14 +1,14 @@
 /**
- * 对比生成两个文档，推荐使用这种
+ * 对比生成一个文档，这种如果两边的表格结构不一样，就没法还原
  * ignore_tag 不进行对比的标签，格式示例：[{openTag: "table", closeTag: "/table"},{openTag: "span class='sectionnumspan'", closeTag: "/span"}]
  * Diff_Timeout 超时时间，0为不对计算进行限制，
  */
-var HtmlDiff = function() {
+var HtmlDiff2 = function() {
     this.ignore_tag = []
     this.Diff_Timeout = 0
 }
 
-HtmlDiff.prototype.diff_launch = function(html1, html2) {
+HtmlDiff2.prototype.diff_launch = function(html1, html2) {
     var text1 = this.convertTextFromHtml(html1);
     var text2 = this.convertTextFromHtml(html2);
 
@@ -18,30 +18,13 @@ HtmlDiff.prototype.diff_launch = function(html1, html2) {
     var diff = dmp.diff_main(text1, text2, true);
     var ms_end = (new Date).getTime();
 
-    // if (diff.length > 2) {
-    //     dmp.diff_cleanupSemantic(diff);
-    // }
-
     let time = ms_end - ms_start;
 
-    let oldDiff = [];
-    let newDiff = [];
-    for (let i = 0,len=diff.length; i < len; i++) {
-        if (diff[i][0] === 0) {
-            oldDiff.push({...diff[i]})
-            newDiff.push({...diff[i]})
-        } else if (diff[i][0] === -1) {
-            oldDiff.push({...diff[i]})
-        } else {
-            newDiff.push({...diff[i]})
-        }
-    }
-    let oldDiffHtml = this.restoreToHtml(html1, oldDiff)
-    let newDiffHtml = this.restoreToHtml(html2, newDiff)
-    return {time, oldDiffHtml, newDiffHtml}
+    let diffHtml = this.restoreToHtml(html2, diff)
+    return {time, diffHtml}
 }
 
-HtmlDiff.prototype.restoreToHtml = function(originalHtml, diffResultList) {
+HtmlDiff2.prototype.restoreToHtml = function(originalHtml, diffResultList) {
     let diffHtml = ''
     while (true) {
         let {tag, text} = this.getOneTextFromHtml(originalHtml)
@@ -50,6 +33,13 @@ HtmlDiff.prototype.restoreToHtml = function(originalHtml, diffResultList) {
         for (let i = 0,len=diffResultList.length; i < len; i++) {
             let diffType = diffResultList[i][0]
             let diffText = diffResultList[i][1]
+            if (diffType === -1) {
+                diffHtml += this.formatText(diffType, diffText)
+                diffResultList.splice(i,1)
+                i--
+                len--
+                continue
+            }
             if (diffText === text) {
                 diffHtml += this.formatText(diffType, diffText)
                 diffResultList.splice(i,1)
@@ -72,10 +62,13 @@ HtmlDiff.prototype.restoreToHtml = function(originalHtml, diffResultList) {
             break
         }
     }
+    for (let i = 0,len=diffResultList.length; i < len; i++) {
+        diffHtml += this.formatText(diffResultList[i][0], diffResultList[i][1])
+    }
     return diffHtml + originalHtml
 }
 
-HtmlDiff.prototype.convertTextFromHtml = function(html) {
+HtmlDiff2.prototype.convertTextFromHtml = function(html) {
     let text = ''
     let tagFlag = false
     this.ignore_tag.map(item => {
@@ -111,7 +104,7 @@ HtmlDiff.prototype.convertTextFromHtml = function(html) {
     return text
 }
 
-HtmlDiff.prototype.getOneTextFromHtml = function(html) {
+HtmlDiff2.prototype.getOneTextFromHtml = function(html) {
     let tag = ''
     let text = ''
     let tagFlag = false
@@ -154,7 +147,7 @@ HtmlDiff.prototype.getOneTextFromHtml = function(html) {
     return {tag, text}
 }
 
-HtmlDiff.prototype.formatText = function(diffType, diffText) {
+HtmlDiff2.prototype.formatText = function(diffType, diffText) {
     if (diffType === 0) {
         return diffText
     } else if (diffType === -1) {
@@ -164,4 +157,4 @@ HtmlDiff.prototype.formatText = function(diffType, diffText) {
     }
 }
 
-this.HtmlDiff = HtmlDiff;
+this.HtmlDiff2 = HtmlDiff2
